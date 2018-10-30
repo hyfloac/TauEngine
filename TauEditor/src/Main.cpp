5501fa20e37788c8b59fcd2fa6b92d2a7d411df7
Unknown <hyfloac@gmail.com>
2018-10-29 22:12:14 -0500

    Initial Commit

---
+++ TauEditor/src/Main.cpp
@@ -0,0 +1,6 @@
+
+
+int main(int argCount, char* args[])
+{
+    
+}
---
+++ TauUtils/include/Maths.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#define RADIANS_TO_DEGREES_CONVERTER_VAL 57.29577951308232087679815481410517
+#define DEGREES_TO_RADIANS_CONVERTER_VAL 0.017453292519943295769236907684886
+
+#define RAD_2_DEG_F(__F) (float) (__F * RADIANS_TO_DEGREES_CONVERTER_VAL##f)
+#define DEG_2_RAD_F(__F) (float) (__F * DEGREES_TO_RADIANS_CONVERTER_VAL##f)
+#define RAD_2_DEG_D(__D) (double) (__D * RADIANS_TO_DEGREES_CONVERTER_VAL)
+#define DEG_2_RAD_D(__D) (double) (__D * DEGREES_TO_RADIANS_CONVERTER_VAL)
+
+#define RAD_2_DEG(__F) RAD_2_DEG_F(__F)
+#define DEG_2_RAD(__F) DEG_2_RAD_F(__F)
+
+#include <cmath>
+#include "NumTypes.hpp"
+
+template<u32 _Base>
+float logN(float x) noexcept
+{
+    return log(x) * log(_Base);
+}
+
+template<u32 _Base>
+double logN(double x) noexcept
+{
+    return log(x) * log(_Base);
+}
+
+template<u32 _Base>
+long double logN(long double x) noexcept
+{
+    return log(x) * log(_Base);
+}
