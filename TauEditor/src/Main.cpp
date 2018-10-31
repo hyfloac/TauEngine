6a61926ce6e0046ecf3510e9684834a1cfcdc83e
hyfloac <hyfloac@gmail.com>
2018-10-30 19:12:00 -0500

    Some basic window creation.
    A very basic 800x600 window.

--- TauEditor/src/Main.cpp
+++ TauEditor/src/Main.cpp
@@ -1,6 +1,19 @@
-
+#include "Maths.hpp"
+#include <cstdio>
+#include <TauEngine.hpp>
+#include <Window.hpp>
 
 int main(int argCount, char* args[])
 {
-    
+    printf("Starting Tau Engine Editor\n");
+
+    Window window(800, 600, "Test Window");
+    window.createWindow();
+    window.showWindow();
+
+    while(!tauRunMessageLoop())
+    {
+    }
+
+    return 0;
 }
