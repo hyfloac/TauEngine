f63c803a6ed41069db389343138c9f4ee71ee7de
hyfloac <hyfloac@gmail.com>
2018-10-31 22:49:49 -0500

    Added some stuff to TauEngine
    Added a `tauInit`, this can be used for initializing some basic stuff. You don't want to initialize GLEW here because that needs to be done individually for every context.
    
    Fixed the message loop to be non-blocking.
    
    Added some stuff for determining if the program should exit.

--- TauEngine/include/TauEngine.hpp
+++ TauEngine/include/TauEngine.hpp
@@ -3,6 +3,15 @@
 #include <DLL.hpp>
 
 /**
+ * Returns true if initialization was successful. 
+ */
+TAU_DLL bool tauInit() noexcept;
+
+/**
  * Returns true if the program should exit;
  */
-TAU_DLL bool tauRunMessageLoop() noexcept;
+TAU_DLL void tauRunMessageLoop() noexcept;
+
+TAU_DLL bool tauShouldExit() noexcept;
+
+TAU_DLL void tauExit(int code) noexcept;
--- TauEngine/src/TauEngine.cpp
+++ TauEngine/src/TauEngine.cpp
@@ -1,21 +1,49 @@
 #include <TauEngine.hpp>
 #include <Utils.hpp>
+#include <GL/glew.h>
+
+bool tauInit() noexcept
+{
+    static bool _initializationComplete = false;
+
+    if(!_initializationComplete)
+    {
+        _initializationComplete = true;
+        // const GLenum success = glewInit();
+        // if(success != GLEW_OK)
+        // {
+        //     return false;
+        // }
+    }
+
+    return true;
+}
 
 #ifdef _WIN32
 #include <Windows.h>
 
-bool tauRunMessageLoop() noexcept
+void tauRunMessageLoop() noexcept
 {
     MSG msg;
-    if(!GetMessageA(&msg, null, 0, 0)) { return true; }
+    if(!PeekMessageA(&msg, null, 0, 0, PM_REMOVE)) { return; }
     TranslateMessage(&msg);
     DispatchMessageA(&msg);
+}
 
-    return false;
+volatile static bool shouldExit = false;
+
+bool tauShouldExit() noexcept
+{
+    return shouldExit;
+}
+
+void tauExit(int code) noexcept
+{
+    shouldExit = true;
+    PostQuitMessage(code);
 }
 #else
-bool tauRunMessageLoop() noexcept
+void tauRunMessageLoop() noexcept
 {
-    return false;
 }
 #endif
