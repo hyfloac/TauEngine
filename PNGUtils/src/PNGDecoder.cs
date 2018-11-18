67c2a86418940a185a306770d7bf9a3a01ef06f0
hyfloac <hyfloac@gmail.com>
2018-11-17 21:03:17 -0600

    Worked on a PNG utility.
    This may be for debugging, or maybe for removing useless elements, but probably for working on adding a custom image file format. Maybe a little of each.

---
+++ PNGUtils/src/PNGDecoder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNGUtils
+{
+    public interface IChunk
+    {
+
+    }
+
+    public enum PixelFormat : byte
+    {
+        GrayScale = 0,
+        RGB = 2,
+        Indexed = 3,
+        GrayScaleAlpha = 4,
+        RGBA = 6
+    }
+
+    public struct PNGHeader : IChunk
+    {
+        public uint width;
+        public uint height;
+        public byte bitsPerPixel;
+        public PixelFormat colorType;
+        public byte compression;
+        public byte filter;
+        public byte interlace;
+    }
+
+    public class PNGDecoder
+    {
+        private static readonly byte[] signature = {0x89, (byte) 'P', (byte) 'N', (byte) 'G', (byte) '\r', (byte) '\n', 0x1A, (byte) '\n' };
+        private BinaryReader br;
+
+        public PNGDecoder(Stream stream)
+        {
+            br = new BinaryReader(stream);
+        }
+
+
+    }
+}
