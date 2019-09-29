31f87d2981ba3f7a323f17ed2d0213aaa4c10fc8
hyfloac <hyfloac@gmail.com>
2019-09-29 14:23:34 -0500

    Added `RefCast`
    Added a `RefCast` function that wraps around `std::static_pointer_cast`. This is intended to be used in tandem with `Ref` instead of `std::shared_ptr`.

--- utils/ResourceLib/pch.h
+++ utils/ResourceLib/pch.h
@@ -46,4 +46,8 @@ using Scoped = std::unique_ptr<_T, _D>;
 template<typename _T>
 using Ref = std::shared_ptr<_T>;
 
+template<typename _Out, typename _In>
+[[nodiscard]] Ref<_Out> RefCast(const Ref<_In>& in) noexcept
+{ return std::static_pointer_cast<_Out>(in); }
+
 #endif
