0746ea61958940b962475ff289ac502e54d074df
hyfloac <hyfloac@gmail.com>
2019-09-30 16:41:16 -0500

    Included ResourceLib in projects
    Finally included ResourceLib into TauEngine and TauEditor. Had to disable Pre-Compiled Headers.

--- utils/ResourceLib/pch.h
+++ utils/ResourceLib/pch.h
@@ -25,6 +25,7 @@
 #include <utility>
 #include <chrono>
 #include <memory>
+#include <tuple>
 
 #include <NumTypes.hpp>
 #include <MathDefines.hpp>
@@ -34,21 +35,12 @@
 #include <VariableLengthArray.hpp>
 #include <Alignment.h>
 #include <DynArray.hpp>
-#include <EnumBitFields.hpp>
 #include <String.hpp>
 
 #ifdef _WIN32
 #include <Windows.h>
 #endif
 
-template<typename _T, typename _D = std::default_delete<_T>>
-using Scoped = std::unique_ptr<_T, _D>;
-
-template<typename _T>
-using Ref = std::shared_ptr<_T>;
-
-template<typename _Out, typename _In>
-[[nodiscard]] Ref<_Out> RefCast(const Ref<_In>& in) noexcept
-{ return std::static_pointer_cast<_Out>(in); }
+#include "RSLib.hpp"
 
 #endif
