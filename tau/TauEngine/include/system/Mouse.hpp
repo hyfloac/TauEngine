6dea1f61fdf3d1f364720e8a74ccaffb5f45c7b7
hyfloac <hyfloac@gmail.com>
2019-11-14 12:58:00 -0600

    Minor changes to mouse
    Added the ability to set the visibility.
    Added the ability to get and set the mouse position.

--- tau/TauEngine/include/system/Mouse.hpp
+++ tau/TauEngine/include/system/Mouse.hpp
@@ -6,36 +6,7 @@
 #pragma once
 #include <NumTypes.hpp>
 
-// /**
-//  * A series of flags for use with Mouse Events.
-//  * 
-//  * These value are equivalent to the Win32 API values.
-//  */
-// enum class MouseFlags : u8
-// {
-//     /** The left mouse button is currently pressed.                  */ MF_LEFT_BUTTON_DOWN   = 1 << 0,
-//     /** The right mouse button is currently pressed.                 */ MF_RIGHT_BUTTON_DOWN  = 1 << 1,
-//     /** The shift key is current pressed.                            */ MF_SHIFT_KEY_DOWN     = 1 << 2,
-//     /** The ctrl key is currently pressed.                           */ MF_CTRL_KEY_DOWN      = 1 << 3,
-//     /** The middle mouse button (scroll wheel) is currently pressed. */ MF_MIDDLE_BUTTON_DOWN = 1 << 4,
-//     /** The first side mouse button is currently pressed.            */ MF_X_BUTTON_1_DOWN    = 1 << 5,
-//     /** The second side mouse button is currently pressed.           */ MF_X_BUTTON_2_DOWN    = 1 << 6
-// };
-//
-// enum class MouseEvent : u8
-// {
-//     /** The left mouse button has just been pressed.                   */ ME_LEFT_BUTTON_DOWN = 0,
-//     /** The left mouse button has just been released.                  */ ME_LEFT_BUTTON_UP,
-//     /** The middle mouse button (scroll wheel) has just been pressed.  */ ME_MIDDLE_BUTTON_DOWN,
-//     /** The middle mouse button (scroll wheel) has just been released. */ ME_MIDDLE_BUTTON_UP,
-//     /** The right mouse button has just been pressed.                  */ ME_RIGHT_BUTTON_DOWN,
-//     /** The right mouse button has just been released.                 */ ME_RIGHT_BUTTON_UP,
-//     /** The first side mouse button has just been pressed.             */ ME_X_BUTTON_1_DOWN,
-//     /** The first side mouse button has just been released.            */ ME_X_BUTTON_1_UP,
-//     /** The second side mouse button has just been pressed.            */ ME_X_BUTTON_2_DOWN,
-//     /** The second side mouse button has just been released.           */ ME_X_BUTTON_2_UP,
-//     ME_ERROR = 0xFF
-// };
+class Window;
 
 class Mouse final
 {
@@ -89,6 +60,18 @@ public:
     [[nodiscard]] static bool isButtonDown(Button button) noexcept;
 
     [[nodiscard]] static Pos mousePos() noexcept;
+
+    static void mousePos(Pos pos) noexcept;
+
+    static void mousePos(u32 x, u32 y) noexcept;
+
+    [[nodiscard]] static Pos mousePos(const Window& window) noexcept;
+
+    static void mousePos(const Window& window, Pos pos) noexcept;
+
+    static void mousePos(const Window& window, u32 x, u32 y) noexcept;
+
+    static void setVisible(bool visible) noexcept;
 
     [[nodiscard]] static int convertToSysVButton(Button key) noexcept;
 
--- tau/TauEngine/src/win32/Win32Mouse.cpp
+++ tau/TauEngine/src/win32/Win32Mouse.cpp
@@ -1,8 +1,9 @@
 #if defined(_WIN32)
 #include "system/Mouse.hpp"
+#include "system/Window.hpp"
 #include <Windows.h>
 
-bool Mouse::isButtonDown(Mouse::Button button) noexcept
+bool Mouse::isButtonDown(const Mouse::Button button) noexcept
 {
     return GetKeyState(Mouse::convertToSysVButton(button)) & 0x8000;
 }
@@ -14,21 +15,59 @@ Mouse::Pos Mouse::mousePos() noexcept
     return { static_cast<u32>(p.x), static_cast<u32>(p.y) };
 }
 
-int Mouse::convertToSysVButton(Mouse::Button key) noexcept
+void Mouse::mousePos(const Pos pos) noexcept
+{
+    SetCursorPos(pos.x, pos.y);
+}
+
+void Mouse::mousePos(const u32 x, const u32 y) noexcept
+{
+    SetCursorPos(x, y);
+}
+
+Mouse::Pos Mouse::mousePos(const Window& window) noexcept
+{
+    POINT p;
+    GetCursorPos(&p);
+    ScreenToClient(window._windowContainer.windowHandle, &p);
+    return { static_cast<u32>(p.x), static_cast<u32>(p.y) };
+}
+
+void Mouse::mousePos(const Window& window, const Pos pos) noexcept
+{
+    POINT p = { static_cast<LONG>(pos.x), static_cast<LONG>(pos.y) };
+    ClientToScreen(window._windowContainer.windowHandle, &p);
+    SetCursorPos(p.x, p.y);
+}
+
+void Mouse::mousePos(const Window& window, const u32 x, const u32 y) noexcept
+{
+    POINT p = { static_cast<LONG>(x), static_cast<LONG>(y) };
+    ClientToScreen(window._windowContainer.windowHandle, &p);
+    SetCursorPos(p.x, p.y);
+}
+
+void Mouse::setVisible(bool visible) noexcept
+{
+    ShowCursor(visible);
+}
+
+
+int Mouse::convertToSysVButton(const Mouse::Button key) noexcept
 {
     switch(key)
     {
-        case  Mouse::Button::LeftButton:  return VK_LBUTTON;
-        case  Mouse::Button::RightButton:  return VK_RBUTTON;
+        case  Mouse::Button::LeftButton:    return VK_LBUTTON;
+        case  Mouse::Button::RightButton:   return VK_RBUTTON;
         case  Mouse::Button::MiddleButton:  return VK_MBUTTON;
-        case  Mouse::Button::XButton1: return VK_XBUTTON1;
-        case  Mouse::Button::XButton2: return VK_XBUTTON2;
+        case  Mouse::Button::XButton1:      return VK_XBUTTON1;
+        case  Mouse::Button::XButton2:      return VK_XBUTTON2;
         default: break;
     }
     return -1;
 }
 
-Mouse::Button Mouse::convertFromSysVButton(int key) noexcept
+Mouse::Button Mouse::convertFromSysVButton(const int key) noexcept
 {
     switch(key)
     {
