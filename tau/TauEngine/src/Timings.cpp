81f043373aa383b0aeb15c67fba5eb0577bb42d4
hyfloac <hyfloac@gmail.com>
2019-12-19 15:27:13 -0600

    Added a delta time class
    Used to store delta time in multiple units and have cumulative delta.

--- tau/TauEngine/src/Timings.cpp
+++ tau/TauEngine/src/Timings.cpp
@@ -271,3 +271,31 @@ void PerfTimer::stop() noexcept
     TimingsWriter::write(TimingsWriter::ProfileResult(_name, threadID, _start, end));
     _stopped = true;
 }
+
+
+DeltaTime::DeltaTime() noexcept
+    : _us(0.0f), _ms(0.0f), _s(0.0f),
+      _usSinceLastUpdate(0.0f), _msSinceLastUpdate(0.0f), _sSinceLastUpdate(0.0f),
+      _usSinceLaunch(0.0f), _msSinceLaunch(0.0f), _sSinceLaunch(0.0f)
+{ }
+
+void DeltaTime::setDeltaMicro(float us) noexcept
+{
+    _us = us;
+    _ms = _us / 1000.0f;
+    _s = _us / 1000000.0f;
+    _usSinceLastUpdate += us;
+    _msSinceLastUpdate = _usSinceLastUpdate / 1000.0f;
+    _sSinceLastUpdate = _usSinceLastUpdate / 1000000.0f;
+    _usSinceLaunch += us;
+    _msSinceLaunch = _usSinceLaunch / 1000.0f;
+    _sSinceLaunch = _usSinceLaunch / 1000000.0f;
+}
+
+void DeltaTime::onUpdate() noexcept
+{
+    _usSinceLastUpdate = 0.0f;
+    _msSinceLastUpdate = 0.0f;
+    _sSinceLastUpdate = 0.0f;
+}
+
