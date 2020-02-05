3a7967cd593d06cc1b5734e202d20cdbb48929c0
hyfloac <hyfloac@gmail.com>
2020-02-04 21:43:49 -0600

    Removed unused Error enums from buffer args

--- tau/TauEngine/include/model/Buffer.hpp
+++ tau/TauEngine/include/model/Buffer.hpp
@@ -133,107 +133,6 @@ struct BufferArgs final
     DEFAULT_DESTRUCT(BufferArgs);
     DEFAULT_COPY(BufferArgs);
 public:
-    enum class Error : u8
-    {
-        NoError = 0,
-        /**
-         * Attempted to allocate an index buffer.
-         *
-         *   One should use IIndexBufferBuilder and IIndexBuffer. This
-         * is because there are special optimization that have been made
-         * based on the usage case.
-         */
-        BufferCannotBeIndexBuffer,
-        /**
-         * Attempted to allocate a uniform buffer.
-         *
-         *   One should use IUniformBufferBuilder and IUniformBuffer. This
-         * is because there are special optimization that have been made
-         * based on the usage case.
-         */
-         BufferCannotBeUniformBuffer,
-        /**
-         * The buffer type is unset.
-         *
-         *   This needs to be set for TauEngine, independent of whether
-         * or not it actually makes a difference in the driver.
-         */
-        TypeIsUnset,
-        /**
-         * The usage type is unset.
-         *
-         *   This needs to be set for TauEngine, independent of whether
-         * or not it actually makes a difference in the driver.
-         */
-        UsageIsUnset,
-        /**
-         * The buffer size was zero.
-         *
-         *   While a driver may support allocating a zero byte buffer,
-         * this serves little practical purpose AFAIK, as such is
-         * disallowed by TauEngine.
-         */
-        BufferSizeIsZero,
-        /**
-         * Failed to allocate system memory.
-         *
-         *   This error is produced when a call to the system memory
-         * allocator (malloc, new, new[], HeapAlloc, LocalAlloc,
-         * GlobalAlloc) fails (returns null). This generally occurs
-         * when the system has run out of memory. It may also mean
-         * that the process itself has exceed its maximum allotted
-         * memory. It may also occur when allocating a buffer that
-         * is too large.
-         *
-         *   If this occurs its probably safe to assume that the
-         * program or system will crash shortly. As such I prescribe to
-         * the philosophy of simply exiting, or outright ignoring the
-         * error, given that it likely won't matter in a couple of seconds
-         * at most.
-         */
-        SystemMemoryAllocationFailure,
-        /**
-         * The driver failed to allocate system memory.
-         *
-         *   This occurs when the driver failed to allocate memory on
-         * the system. This is likely caused by the same reasons as
-         * Error::SystemMemoryAllocationFailure.
-         */
-        DriverMemoryAllocationFailure,
-        /**
-         * The driver failed to allocate memory on the GPU.
-         *
-         *   This occurs when the GPU runs out of memory. It may also mean
-         * that there isn't a GPU installed, or at the very least, one that
-         * isn't suitable. This may be more resolvable by scaling back
-         * texture sizes and model sizes. As well as deallocating unnecessary
-         * objects. Depending on the backing API it may also be possible to
-         * move certain allocation from the GPU back into system memory.
-         */
-        GPUMemoryAllocationFailure,
-        /**
-         * TauEngine screwed up somehow.
-         *
-         * I fucked up. Tell me about.
-         *
-         *   This shouldn't ever occur. If it does I must have done something
-         * very wrong. Please, tell me about it, including any relevant
-         * information - things like the call stack (I mostly care about within
-         * TauEngine), the model (or at the very least, the model size), etc.
-         */
-        LibImplementationError,
-        /**
-         * An unknown error occured.
-         *
-         *   This is also likely my fault. It will generally occur if a driver
-         * returns an undocumented error code. If you can debug the function
-         * I would like to know what the exact error code given by the driver
-         * is, if you are also able to identify what the name of the error is
-         * (like the macro value) that may also be useful.
-         */
-        UnknownError,
-    };
-public:
     EBuffer::Type type;
     EBuffer::UsageType usage;
     uSys elementCount;
@@ -257,8 +156,6 @@ struct IndexBufferArgs final
     DEFAULT_DESTRUCT(IndexBufferArgs);
     DEFAULT_COPY(IndexBufferArgs);
 public:
-    using Error = BufferArgs::Error;
-public:
     EBuffer::UsageType usage;
     uSys elementCount;
     void* initialBuffer;
@@ -275,8 +172,6 @@ struct UniformBufferArgs final
 {
     DEFAULT_DESTRUCT(UniformBufferArgs);
     DEFAULT_COPY(UniformBufferArgs);
-public:
-    using Error = BufferArgs::Error;
 public:
     EBuffer::UsageType usage;
     uSys bufferSize;
