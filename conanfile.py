54950394c0160d35f4636a5ae7cc0254319ce437
hyfloac <mail@hyfloac.com>
2025-09-15 11:17:53 -0500

    Added more dependencies
    Added GLEW, FreeImage, and OpenVR through Conan.

--- conanfile.py
+++ conanfile.py
@@ -16,11 +16,14 @@ class TauEngineRecipe(ConanFile):
         "tauutils/[^1.4.2]", 
         "taucom/[^0.2.3]",
         "glm/[^1.0.1]",
+        "glew/[^2.2.0]",
         "vulkan-memory-allocator/[^3.0.1]",
         "eastl/[^3.21.12]",
         "tomlplusplus/[^3.4.0]",
         "freetype/2.13.2",
-        "fontconfig/[^2.15.0]"
+        "fontconfig/[^2.15.0]",
+        "freeimage/[^3.18.0]",
+        "openvr/[^1.16.8]"
     )
     options = {
         "shared": [True, False],
@@ -29,21 +32,29 @@ class TauEngineRecipe(ConanFile):
         "shared": False,
     }
 
+    def configure(self):
+        self.options["tauutils"].shared = True
+
+        self.options["taucom"].shared = True
+        self.options["taucom"].useTauUtils = True
+
+        self.options["glew"].shared = True
+
     def layout(self):
         # self.folders.generators = "libs/conan"
         cmake_layout(self)
 
     def generate(self):
         ms = MSBuildDeps(self)
-        ms.generate()
+        # ms.generate()
         if self.settings.build_type == "Debug":
             ms.configuration = "OptimizedDebug"
-            ms.generate()
+            # ms.generate()
         if self.settings.build_type == "Release":
             ms.configuration = "Production"
-            ms.generate()
+            # ms.generate()
             ms.configuration = "TRG_Release"
-            ms.generate()
+            # ms.generate()
             
         deps = CMakeDeps(self)
         deps.generate()
